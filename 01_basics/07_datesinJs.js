eceb05d870cb839616355a6e0a4eecb5d6b0b40a
Priyanshri Priyadarshini Panda <priyanshripriyadarshinipanda30@gmail.com>
2026-06-16 18:08:48 +0000

    01_basic commits

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,32 +1,40 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate); //2026-06-16T18:05:10.531Z
+console.log(myDate.toString()); // Tue Jun 16 2026 18:05:10 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Tue Jun 16 2026
+console.log(myDate.toLocaleString()); // 6/16/2026, 6:05:10 PM
+console.log(typeof myDate);//object
+
+
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString()); // 1/14/2023, 12:00:00 AM
+
+
+
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp); //1781633110554
+console.log(myCreatedDate.getTime()); //1673654400000
+console.log(Math.floor(Date.now()/1000)); //1781633110
+
+
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate); //2026-06-16T18:05:10.554Z
+console.log(newDate.getMonth() + 1); //6
+console.log(newDate.getDay()); //2
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
+console.log(newDate.toLocaleString('default', {
     weekday: "long",
     
-})
+})) //Tuesday
 
