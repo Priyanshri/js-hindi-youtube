a310871a146e85d0f41d271c4349987153454088
Priyanshri Priyadarshini Panda <priyanshripriyadarshinipanda30@gmail.com>
2026-06-16 18:38:51 +0000

    array commit

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,10 +1,14 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5]
+const myArr = [0, 1, 2, 3, 4, 5] // can have different data type elements
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(myArr[1]); // 1
+
+
+
 
 // Array methods
 
@@ -12,28 +16,29 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) // addes element in 1st position
+// myArr.shift() // pops from 1st position
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9)); // false
+console.log(myArr.indexOf(3)); // 3
 
-// const newArr = myArr.join()
+const newArr = myArr.join() //makes the array elements to string
 
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
+console.log( newArr); // 0,1,2,3,4,5
+console.log( typeof newArr); // string
 
 
-// slice, splice
 
-console.log("A ", myArr);
+// slice, splice(modifies the array)
 
+console.log("A ", myArr); // A  [ 0, 1, 2, 3, 4, 5 ]
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+console.log(myn1);// [ 1, 2 ]
+console.log("B ", myArr); // B  [ 0, 1, 2, 3, 4, 5 ]
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+console.log("C ", myArr); // C  [ 0, 4, 5 ]
+console.log(myn2); // [ 1, 2, 3 ]
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -3,29 +3,44 @@ const dc_heros = ["superman", "flash", "batman"]
 
 // marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros); // [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]); // flash
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+const allHeros = marvel_heros.concat(dc_heros) //(in concat can't merge more than 2 arrays)
+console.log(allHeros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // spread arrays(can merge more than 2 arrays)
+
+console.log(all_new_heros); // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+console.log(real_another_array); 
+/* [
+  1, 2, 3, 4, 5,
+  6, 7, 6, 7, 4,
+  5
+]
+  */
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) // [ 'H', 'i', 't', 'e', 's', 'h' ]
+//Array.from : Creates an array from an iterable object.
+console.log(Array.from({name: "hitesh"})) //op : []  // need to specify which should be converted thekey or value
+
+
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); //[ 100, 200, 300 ] 
+//Array.of : returns a new array from set of elements
