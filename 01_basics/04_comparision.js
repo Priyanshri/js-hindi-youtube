ee43ad6eecf9b6a175cc77019bd28d0f5db043ff
Priyanshri Priyadarshini Panda <priyanshripriyadarshinipanda30@gmail.com>
2026-06-16 17:21:00 +0000

    changed  in 04 and datatype summ

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,17 +5,18 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+//The reason is that an equality check == and comparisons > <>= <= work differently.Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false.
+console.log(null > 0); //false
+console.log(null == 0);//false
+console.log(null >= 0); //true
 
-// === 
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
 
+// === : strict check 
 console.log("2" === 2);
