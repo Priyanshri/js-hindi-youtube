3518c76675648c5b146879e172d8050ece709e17
Priyanshri Priyadarshini Panda <priyanshripriyadarshinipanda30@gmail.com>
2026-06-17 09:57:00 +0000

    object commit

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,7 +9,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1", //to access symbol we use []
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,15 +17,31 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+console.log(JsUser.email) // hitesh@google.com
+console.log(JsUser["email"]) // hitesh@google.com // another method to access object members
+console.log(JsUser["full name"]) //Hitesh Choudhary
+console.log(JsUser[mySym]) // mykey1
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser) //this will freeze all the members of that object and we further can't change it.
 JsUser.email = "hitesh@microsoft.com"
+//after freezing
 // console.log(JsUser);
+/*
+{
+  name: 'Hitesh',
+  'full name': 'Hitesh Choudhary',
+  age: 18,
+  location: 'Jaipur',
+  email: 'hitesh@chatgpt.com',
+  isLoggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  [Symbol(key1)]: 'mykey1'
+}
+*/ 
+
+
+
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
@@ -35,4 +51,12 @@ JsUser.greetingTwo = function(){
 }
 
 console.log(JsUser.greeting());
+/* 
+Hello JS user
+undefined
+*/
 console.log(JsUser.greetingTwo());
+/*
+Hello JS user, Hitesh
+undefined
+*/
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,11 +1,11 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() // singleton object
+const tinderUser = {} //non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 const regularUser = {
     email: "some@gmail.com",
@@ -17,17 +17,24 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname?.userfullname.firstname); // optional chaining :checks whether the fullname exists or not
+
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
+// console.log(obj3);
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
+// console.log(obj3);
 
 const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+console.log(obj3);
+
+
 
 
 const users = [
@@ -46,13 +53,17 @@ const users = [
 ]
 
 users[1].email
-// console.log(tinderUser);
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//checks if there is any property or not in the object
+
+
+
+//********Destructuring*******
 
 
 const course = {
@@ -63,20 +74,30 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course //Destructuring the value courseInstructor by using {}
 
-// console.log(courseInstructor);
-console.log(instructor);
+console.log(instructor); 
 
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
 
+
+// JSON format types when api fetches
+
+/*
+1.
+{
+    "name": "hitesh",
+    "coursename": "js in hindi",
+    "price": "free"
+}
+*/
+/*
+2.
 [
     {},
     {},
     {}
 ]
+    */
 
+
+// we can see randomuser api that is free and break down that and see the json structure in json formatter website.
