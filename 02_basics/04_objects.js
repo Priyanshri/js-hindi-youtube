36aff8629eedbdf080863b531b808c05d5a62a06
Priyanshri Priyadarshini Panda <priyanshripriyadarshinipanda30@gmail.com>
2026-06-17 10:06:10 +0000

    obj cmt 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,7 +5,7 @@ tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
@@ -17,9 +17,9 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname);
-console.log(regularUser.fullname.userfullname.firstname);
-console.log(regularUser.fullname?.userfullname.firstname); // optional chaining :checks whether the fullname exists or not
+console.log(regularUser.fullname); // { userfullname: { firstname: 'hitesh', lastname: 'choudhary' } }
+console.log(regularUser.fullname.userfullname.firstname); // hitesh
+console.log(regularUser.fullname?.userfullname.firstname); // hitesh  // optional chaining :checks whether the fullname exists or not
 
 
 const obj1 = {1: "a", 2: "b"}
@@ -27,12 +27,13 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// console.log(obj3);
+// console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
-// console.log(obj3);
+// console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 
 
@@ -53,13 +54,13 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser)); // [ '123abc', 'Sammy', false ]
+console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));//checks if there is any property or not in the object
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true   //checks if there is any property or not in the object
 
 
 
@@ -75,8 +76,7 @@ const course = {
 // course.courseInstructor
 
 const {courseInstructor: instructor} = course //Destructuring the value courseInstructor by using {}
-
-console.log(instructor); 
+console.log(instructor);  // hitesh
 
 
 
